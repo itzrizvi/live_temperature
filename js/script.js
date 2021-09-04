6baa3f4da698402f003a2c728743d57a827a41e2
itzrizvi <shahriar.me.work@gmail.com>
2021-09-04 17:39:12 +0600

    REVIEW1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const onLoadDefault = () => {
     const myApiKey = `91105755f287ef850ce43c786caf3a00`;
-    const apiURLforDefault = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=${myApiKey}`;
+    const apiURLforDefault = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=${myApiKey}&units=metric`;
 
     fetch(apiURLforDefault)
         .then(res => res.json())
@@ -8,18 +8,12 @@ const onLoadDefault = () => {
 }
 
 const defaultWeather = (data) => {
-    const tempKelvinToCelcius = parseFloat(data.main.temp) - 273.15;
-    const mainTemp = tempKelvinToCelcius.toFixed(2);
-
-    const templFLKelToCel = parseFloat(data.main.feels_like) - 273.15;
-    const mainFeelsLike = templFLKelToCel.toFixed(2);
-
     const weatherOutputCont = document.getElementById('weather-output');
     weatherOutputCont.innerHTML =
         `   <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
             <h1>${data.name}</h1>
-            <h3><span>${mainTemp}</span>&deg;C</h3>
-            <h3>Feels Like <span>${mainFeelsLike}</span>&deg;C</h3>
+            <h3><span>${data.main.temp}</span>&deg;C</h3>
+            <h3>Feels Like <span>${data.main.feels_like}</span>&deg;C</h3>
             <h1 class="lead">${data.weather[0].description.toUpperCase()}</h1>
         `;
 }
@@ -28,25 +22,19 @@ const loadApi = () => {
     const getCityName = getCityInput.value.toLowerCase();
     getCityInput.value = '';
     const myApiKey = `91105755f287ef850ce43c786caf3a00`;
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${getCityName}&appid=${myApiKey}`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${getCityName}&appid=${myApiKey}&units=metric`;
 
     fetch(apiURL)
         .then(response => response.json())
         .then(weatherdata => showWeatherData(weatherdata));
 }
 const showWeatherData = (weatherData) => {
-    const tempKelvinToCelcius = parseFloat(weatherData.main.temp) - 273.15;
-    const mainTemp = tempKelvinToCelcius.toFixed(2);
-
-    const templFLKelToCel = parseFloat(weatherData.main.feels_like) - 273.15;
-    const mainFeelsLike = templFLKelToCel.toFixed(2);
-
     const weatherOutputCont = document.getElementById('weather-output');
     weatherOutputCont.innerHTML =
         `   <img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png" alt="">
             <h1>${weatherData.name}</h1>
-            <h3><span>${mainTemp}</span>&deg;C</h3>
-            <h3>Feels Like <span>${mainFeelsLike}</span>&deg;C</h3>
+            <h3><span>${weatherData.main.temp}</span>&deg;C</h3>
+            <h3>Feels Like <span>${weatherData.main.feels_like}</span>&deg;C</h3>
             <h1 class="lead">${weatherData.weather[0].description.toUpperCase()}</h1>
         `;
 }
